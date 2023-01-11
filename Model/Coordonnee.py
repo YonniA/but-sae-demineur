b757fe82fa60f3e0c52939bf954d3392a6c6c4a0
Yonni Andreu <yonni.andreu@etudiant.univ-reims.fr>
2023-01-11 12:20:50 +0100

    Coordonnées implémentées : test ok

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -2,6 +2,7 @@
 
 import const
 
+
 # Définition des coordonnées (ligne, colonne)
 
 
@@ -19,3 +20,54 @@ def type_coordonnee(coord: tuple) -> bool:
         and coord[0] >= 0 and coord[1] >= 0
 
 
+def construireCoordonnee(num_li: int, num_co: int) -> tuple:
+    """
+
+    :param num_li: int
+    :param num_co: int
+    :return: Forme un tuple à partir de deux entiers positifs, si négatif ou pas entier: renvoie une erreur
+    """
+    if (type(num_li) or type(num_co)) != int:
+        raise TypeError(
+            f"construireCoordonnee : Le numéro de ligne {type(num_li)} ou le numéro de colonne {type(num_co)} ne sont pas des entiers.")
+    elif num_li < 0 or num_co < 0:
+        raise ValueError(
+            f"construireCoordonnee : Le numéro de ligne ({num_li}) ou le numéro de colonne ({num_co}) ne sont pas positifs.")
+    else:
+        return num_li, num_co
+
+def estCoordonnee(coord) -> bool:
+    """
+
+    :param coord: tuple
+    :return: Renvoie True si le paramètre est une coordonnée valide, sinon False
+    """
+    res = False
+    if type(coord) == tuple:
+        if (type(coord[0]) and type(coord[1])) == int and (coord[0] and coord[1]) >= 0:
+            res = True
+    return res
+
+def getLigneCoordonnee(coord: tuple) -> int:
+    """
+
+    :param coord: tuple
+    :return: Renvoie le numéro de colonne si coord est une coordonnée, sinon renvoie une erreur
+    """
+    if not estCoordonnee(coord):
+        raise TypeError("getLigneCoordonnee : Le paramètre n'est pas une coordonnée")
+    else:
+        return coord[0]
+
+
+def getColonneCoordonnee(coord: tuple) -> int:
+    """
+
+
+    :param coord: tuple
+    :return: Renvoie le numéro de colonne si coord est une coordonnée, sinon renvoie une erreur
+    """
+    if not estCoordonnee(coord):
+        raise TypeError("getCoordCoordonnee : Le paramètre n'est pas une coordonnée")
+    else:
+        return coord[1]
